ff81b5b0e033efdbd0e1a51457eaf0f3321ddba2
minmax329 <964218092@qq.com>
2025-02-22 00:17:52 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1a16ab79fe9b80f69fb9e4459598c7dc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'eatwelltherapy',
+  domain: 'eatwelltherapy.com',
+  author: 'Cloud Wearm',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
