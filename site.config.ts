3d5f605545ead3eccb5fa9e39a01e2e0a778f3c9
minmax329 <964218092@qq.com>
2025-02-23 23:12:51 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Cloud Wearm',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'EatWellTherapy Site',
 
   // social usernames (optional)
   twitter: null,
